7b10b44cdf38b4cf7c7f388d15f3d511e150edc9
sahmedroni <roni.dboy@gmail.com>
2022-08-15 14:30:30 +0600

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function myClock() //function declared
     let hh = date.getHours();   //get only hour from date() fumction.
     let mm = date.getMinutes(); //get only minute from date() fumction.
     let ss = date.getSeconds(); //get only secons from date() fumction.
-    let session = "AM"; //variable to hold the AM PM
+    let session = document.getElementById("sess").className = "sess fa fa-cloud";; //variable to hold the AM PM
 
 // condition to convert 24 hours to 12 hours clock.
 if (hh == 0) {
@@ -14,30 +14,31 @@ if (hh == 0) {
 
 // it will calculate 24 hours time to 12 hours time mode.
 if (hh > 12) {
-    hh = hh-12;
-    session = "PM";
+  hh = hh-12;
+session = "PM";
 }
+
+//if (session == "PM") {
+//document.getElementById('ico').className = "fa-light fa-sun";
+//}
+
 
 // to add a 0 before single digits.
 hh = (hh < 10) ? "0" + hh : hh;
 mm = (mm < 10) ? "0" + mm : mm;
 ss = (ss < 10) ? "0" + ss : ss;
 
-
+//document.getElementById("sess").innerHTML = session;
+if (session=="PM") {
+    session = document.getElementById("sess").className = "sess fa fa-sun-o";
+}
 
 let time = hh + "  " + mm + "  " + ss; //forming the time and holding in a variable
 document.getElementById("clock").innerHTML = time;  // display the final time to the html tag
-document.getElementById('sess').innerHTML = session;
+
 setTimeout(myClock,1000);
 
-//alarm section
-let alarmHour 
-if (hh == document.getElementById("sethour").value && 
-mm == document.getElementById("setminute").value) {
-    document.getElementById("alarm").innerHTML = "Wakeup";
-    document.getElementById("alarm").style.backgroundColor = "Red";
-}
-else document.getElementById("alarm").innerHTML = "Alarm";
+
 
 }
 myClock();
