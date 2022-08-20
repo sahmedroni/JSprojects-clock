7baa2a6c470de3e1da03147a2dd016e662eabf49
sahmedroni <roni.dboy@gmail.com>
2022-08-20 20:30:06 +0600

    modified:   style.css

--- script.js
+++ script.js
@@ -7,21 +7,15 @@ function myClock() //function declared
     let ss = date.getSeconds(); //get only secons from date() fumction.
     let session = "AM"; //variable to hold the AM PM
 
-// condition to convert 24 hours to 12 hours clock.
+//convert 24 hours to 12 hours.
 if (hh == 0) {
     hh = 12;
 }
 
-// it will calculate 24 hours time to 12 hours time mode.
 if (hh > 12) {
   hh = hh-12;
 session = "PM";
 }
-
-//if (session == "PM") {
-//document.getElementById('ico').className = "fa-light fa-sun";
-//}
-
 
 // to add a 0 before single digits.
 hh = (hh < 10) ? "0" + hh : hh;
@@ -29,17 +23,11 @@ mm = (mm < 10) ? "0" + mm : mm;
 ss = (ss < 10) ? "0" + ss : ss;
 
 document.getElementById("sess").innerHTML = session;
-//if (session=="PM") {
- //   session = document.getElementById("sess").className = "sess fa fa-sun-o";
-//}
 
 let time = hh + ":" + mm + ":" + ss; //forming the time and holding in a variable
 document.getElementById("clock").innerHTML = time;  // display the final time to the html tag
 
 setTimeout(myClock,1000);
-
-
-
 }
 myClock();
 
